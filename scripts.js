8d8f9a43144f76d1f9f893b35a9edbb99ed90ab4
Stephen Cheng <aw3someone@gmail.com>
2017-10-27 16:54:39 -0700

    improved performance

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ var file
 file = "https://raw.githubusercontent.com/Noverdose/Preventanyl-Server/develop/canada/admin_level_2.geojson"
 file = "https://raw.githubusercontent.com/Noverdose/Preventanyl-Server/develop/canada/admin_level_8.geojson"
 function getFile(fn){
-    console.log('before')
     $.get(fn, function(data) {    
         var lines = data.split("\n");
 
@@ -10,12 +9,12 @@ function getFile(fn){
             try {
                 let json = JSON.parse(elem)
                 if (typeof json['name'] !== 'undefined' && typeof json['geometry']['coordinates'][0] !== 'undefined' && json["osm_type"] == "way") {
-                    addLocation(json['name'], json['geometry']['coordinates'][0])
+                    // addLocation(json['name'], json['geometry']['coordinates'][0])
+                    addLocationV2(json['name'], json['geometry']['coordinates'][0])
                 }
             } catch (e) {}
         });
     });    
-    console.log('after')
 }
 
 function addLocation(name, coordinates) {
@@ -31,4 +30,20 @@ function addLocation(name, coordinates) {
     }
 }
 
+function addLocationV2(name, coordinates) {
+    var c = []
+    c['coordinates'] = []
+    for (let i = 0; i < coordinates[0].length; i++) {
+        var t = []
+        t['lat'] = coordinates[0][i][0]
+        t['long'] = coordinates[0][i][1]
+        c['coordinates'].push(t)
+    }
+    firebase.database().ref('/locations/' + name).set({
+        name: name,
+        geometry: c
+    })
+}
+
+firebase.database().ref('/locations/').set(null)
 getFile(file)
